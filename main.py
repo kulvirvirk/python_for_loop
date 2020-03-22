30d04fd1825820f6cda1a0409b255ad33de5969e
kulvirvirk <kvirkdev@gmail.com>
2020-03-22 02:59:24 +0000

    added for loop eg3: loop adds all the numbers from 0 to 100

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 # 1. print name five times
+# 2. print name five times but start with 1. not 0. 
+# 3. using a for loop - add numbers from 0 to 100
 
+# 1. print name five times
 for x in range(5):
 	print(str(x) + '. My name is Singh!')
-print('-------------*********-------------')
+print('-------------*********-------------\n')
 
+# 2. print name five times but start with 1. not 0.
 for x in range(5):
 	print(str(x+1) + '. My name is Singh!')
-print('-------------*********-------------')
+print('-------------*********-------------\n')
 
-
-
+# 3. using a for loop - add numbers from 1 to 100
+total = 0
+for num in range(101):
+  total = total + num; 
+print('The sum from all number form 0 to 100 is: ' + str(total))
+print('-------------*********-------------\n')
